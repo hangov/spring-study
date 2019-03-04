ab78cd46ff40cd5aff5226b60b9829e8c41e7da5
hangov <1013818707@qq.com>
2019-03-04 18:08:47 +0800

    new study

---
+++ IoC/src/main/java/com/spring/spring/quickstart/UserLogin.java
@@ -0,0 +1,33 @@
+package com.spring.spring.quickstart;
+
+public class UserLogin {
+    private User user;
+
+    public UserLogin(User user) {
+    }
+
+    public UserLogin() {
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public boolean userLogin(User user){
+        if (user.getAccount().equals("admin")&& user.getPassword().equals("111")){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "UserLogin{" +
+                "user=" + user +
+                '}';
+    }
+}
---
+++ quickstart/src/main/java/com/spring/spring/quickstart/App.java
@@ -0,0 +1,13 @@
+package com.spring.spring.quickstart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
